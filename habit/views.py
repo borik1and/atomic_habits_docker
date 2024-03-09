eb74e0beae20827ac9e03ff2acefc281f89537f1
Boris <borik1and@gmail.com>
2024-03-09 16:06:19 +0200

    add pagination_class

--- habit/views.py
+++ habit/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from habit.models import Habit
+from habit.paginators import HabitPaginator
 from habit.serializers import HabitSerializer
 
 
@@ -9,3 +10,4 @@ class HabitViewSet(viewsets.ModelViewSet):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
     permission_classes = [IsAuthenticated]
+    pagination_class = HabitPaginator
