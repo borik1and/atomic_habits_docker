a6f4912ae2087dcc4dfb427e21eb3ef9cc1295ce
Boris <borik1and@gmail.com>
2024-03-10 16:23:54 +0200

    changing code

--- users/urls.py
+++ users/urls.py
@@ -11,6 +11,6 @@ router.register(r'user', UserViewSet, basename='user')
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users/register/', UserRegistrationView.as_view(), name='user-registration'),
-    path('users/login/', UserLoginView.as_view(), name='user-login'),
+    path('register/', UserRegistrationView.as_view(), name='registration'),
+    path('login/', UserLoginView.as_view(), name='login'),
 ] + router.urls
