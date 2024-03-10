fa21d1fe4dcae742818fdc9f727af36bf77c9335
Boris <borik1and@gmail.com>
2024-03-10 13:15:40 +0200

    changing code

--- habit/views.py
+++ habit/views.py
@@ -1,31 +1,21 @@
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from habit.models import Habit
 from habit.paginators import HabitPaginator
 from habit.serializers import HabitSerializer
-from habit.services import TelegramBotService
+from habit.permissions import IsOwnerOrReadOnly
 
 
 class HabitViewSet(viewsets.ModelViewSet):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]  # Добавляем кастомное разрешение
     pagination_class = HabitPaginator
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        if self.request.user.is_authenticated:
-            return queryset.filter(self.request.user)
-        else:
-            return queryset.none()
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)  # Указываем текущего пользователя как владельца привычки
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
