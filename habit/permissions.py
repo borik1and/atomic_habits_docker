31c9cd932760a023701d87a794cbdb0ef3302486
Boris <borik1and@gmail.com>
2024-03-10 13:14:16 +0200

    add class IsOwnerOrReadOnly

--- habit/permissions.py
+++ habit/permissions.py
@@ -1,6 +1,15 @@
 from rest_framework import permissions
 
 
-class IsOwner(permissions.BasePermission):
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Пользователь может редактировать или удалять только свои привычки.
+    """
+
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        # Разрешено для чтения для всех запросов если они помечены как публичные.
+        if request.method in permissions.SAFE_METHODS and obj.is_public is True:
+            return True
+
+        # Разрешено для записи только владельцу привычки.
+        return obj.user == request.user
