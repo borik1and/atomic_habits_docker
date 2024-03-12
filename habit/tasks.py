b909bc7285f3919e9b1f57078740e019d8133127
Boris <borik1and@gmail.com>
2024-03-12 19:08:54 +0200

    changing in def check_messages_sending

--- habit/tasks.py
+++ habit/tasks.py
@@ -12,7 +12,9 @@ def check_messages_sending(pk):
     for habit in habits:
         if habit.next_dispatch_time is None or habit.next_dispatch_time == datetime.now():
             telegram_bot = TelegramBotService()
-            telegram_bot.send_message(user.chat_id, f'Напоминание привычки {habit.name}')
+            telegram_bot.send_message(user.chat_id, f'Напоминание привычки {habit.name}сделать в '
+                                                    f'{habit.place}вот это:{habit.action},'
+                                                    f' а потом наградить себя {habit.reward}')
             if habit.next_dispatch_time is None:
                 habit.next_dispatch_time = datetime.now() + timedelta(days=habit.period)
             else:
