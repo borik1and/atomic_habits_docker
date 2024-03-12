ab2d3d7c9972c70f2ba4ba2e798d9d97e8c2603a
Boris <borik1and@gmail.com>
2024-03-12 20:36:42 +0200

    changing code to test telegram bot

--- users/management/commands/telegram_send.py
+++ users/management/commands/telegram_send.py
@@ -6,4 +6,4 @@ from habit.services import TelegramBotService
 class Command(BaseCommand):
     def handle(self, *args, **options):
         my_bot = TelegramBotService()
-        my_bot.send_message('Hi<This id your bot speaking>')
+        my_bot.send_message('1115606350', 'Hi<This id your bot speaking>')
