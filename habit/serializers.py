670bd8e618b2029d0df87e863ac32a422624af4a
Boris <borik1and@gmail.com>
2024-03-12 15:03:51 +0200

    add def validate

--- habit/serializers.py
+++ habit/serializers.py
@@ -7,3 +7,13 @@ class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+
+    def validate(self, attrs):
+        related_habit = attrs.get('related_habit')
+        reward = attrs.get('reward')
+
+        if related_habit and reward:
+            raise serializers.ValidationError(
+                "Нельзя одновременно выбрать связанную привычку и указать вознаграждение.")
+
+        return attrs
