bf1c521b95f50575335a07bc7a587522cf24a579
Boris <borik1and@gmail.com>
2024-03-12 16:45:05 +0200

    changing in habit model

--- habit/models.py
+++ habit/models.py
@@ -1,6 +1,4 @@
 from datetime import timedelta
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from django.db import models
 
@@ -15,18 +13,11 @@ class Habit(models.Model):
     sign_pleasant_habit = models.BooleanField(default=False, verbose_name='Признак приятности привычки')
     related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True,
                                       verbose_name='Связанная привычка')
-    period = models.IntegerField(default=1, verbose_name='Периодичность выполнения')
+    period = models.DurationField(default=timedelta(days=1), verbose_name='Периодичность выполнения')
     reward = models.CharField(max_length=250, blank=True, verbose_name='Вознаграждение')
     time_to_complete = models.DurationField(default=timedelta(minutes=2), verbose_name='Время на выполнение')
     is_public = models.BooleanField(default=False, verbose_name='Признак публичности')
     next_dispatch_time = models.DateTimeField(default=None, **NULLABLE, verbose_name='время следующей отправки')
-
-    def clean(self):
-        if self.related_habit and not self.related_habit.sign_pleasant_habit:
-            raise ValidationError(_('Связанная привычка должна быть приятной.'))
-
-        if self.period < 1 or self.period > 7:
-            raise ValidationError(_('Нельзя устанавливать привычку реже, чем 1 и более 7'))
 
     def __str__(self):
         return self.name
