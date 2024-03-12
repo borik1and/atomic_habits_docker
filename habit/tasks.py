e533492a8b6702970bffd40fcac739ff5746df90
Boris <borik1and@gmail.com>
2024-03-12 19:36:35 +0200

    changing in def check_messages_sending

--- habit/tasks.py
+++ habit/tasks.py
@@ -1,22 +1,24 @@
 from datetime import timedelta, datetime
 from celery import shared_task
+from celery.utils.time import timezone
+
 from habit.services import TelegramBotService
 from habit.models import Habit
-from users.models import User
 
 
 @shared_task
-def check_messages_sending(pk):
-    user = User.objects.get(pk=pk)
-    habits = Habit.objects.filter(pk=pk)
+def check_messages_sending():
+    habits = Habit.objects.filter()
+    current_time = timezone.now()
     for habit in habits:
-        if habit.next_dispatch_time is None or habit.next_dispatch_time == datetime.now():
+        if habit.next_dispatch_time is None or habit.next_dispatch_time == current_time:
             telegram_bot = TelegramBotService()
-            telegram_bot.send_message(user.chat_id, f'Напоминание привычки {habit.name}сделать в '
+            telegram_bot.send_message(habit.user.chat_id, f'Напоминание привычки {habit.name}сделать в '
                                                     f'{habit.place}вот это:{habit.action},'
                                                     f' а потом наградить себя {habit.reward}')
-            if habit.next_dispatch_time is None:
-                habit.next_dispatch_time = datetime.now() + timedelta(days=habit.period)
+            # Проверка, если next_dispatch_time не установлен, либо если оно равно текущему времени
+            if habit.next_dispatch_time is None or habit.next_dispatch_time == current_time:
+                habit.next_dispatch_time = current_time + timedelta(days=habit.period)
             else:
                 habit.next_dispatch_time = habit.next_dispatch_time + timedelta(days=habit.period)
-    Habit.objects.bulk_update(habits, ['next_dispatch_time'])
+            habit.save()
