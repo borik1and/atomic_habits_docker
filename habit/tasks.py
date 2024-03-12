f8ffdc641dd49291e66613663890e492ba8f0b5b
Boris <borik1and@gmail.com>
2024-03-12 15:03:30 +0200

    moving chat_id

--- habit/tasks.py
+++ habit/tasks.py
@@ -2,15 +2,17 @@ from datetime import timedelta, datetime
 from celery import shared_task
 from habit.services import TelegramBotService
 from habit.models import Habit
+from users.models import User
 
 
 @shared_task
 def check_messages_sending(pk):
+    user = User.objects.get(pk=pk)
     habits = Habit.objects.filter(pk=pk)
     for habit in habits:
         if habit.next_dispatch_time is None or habit.next_dispatch_time == datetime.now():
             telegram_bot = TelegramBotService()
-            telegram_bot.send_message(habit.chat_id, f'Напоминание привычки {habit.name}')
+            telegram_bot.send_message(user.chat_id, f'Напоминание привычки {habit.name}')
             if habit.next_dispatch_time is None:
                 habit.next_dispatch_time = datetime.now() + timedelta(days=habit.period)
             else:
